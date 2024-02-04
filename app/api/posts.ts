b3da965a535921b4496c80a7de2ad1524cda5540
virvaldev <valtteri.virtanen.dev@gmail.com>
2024-02-04 19:27:37 +0200

    Database synced with Prisma Schema

---
+++ app/api/posts.ts
@@ -0,0 +1,9 @@
+import sqlite3 from 'sqlite3';
+import { open } from 'sqlite';
+import path from 'path';
+
+const handler = async (req, res) => {
+    const DbPath = path.join(process.cwd(), 'petlife.db');
+}
+
+
---
+++ prisma/migrations/20240204172715_postgres1/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "published" BOOLEAN NOT NULL DEFAULT false,
+    "authorId" INTEGER NOT NULL,
+    CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
